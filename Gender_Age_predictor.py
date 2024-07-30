4effc1cff87ad1b4be43e7a3e1d44b38c3017391
himynameissh <151613960+SooHyunHub@users.noreply.github.com>
2024-07-30 17:09:27 +0900

    Update Gender_Age_predictor.py

--- Gender_Age_predictor.py
+++ Gender_Age_predictor.py
@@ -1,10 +1,12 @@
-#edited
+#edited_7/30_17:08, 나이 정확도 개선 가중치 파일 변경
 
 import cv2
 import math
 import argparse
 import time
 import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
 
 
 class Gender_Age_pridictor:
@@ -16,15 +18,17 @@ class Gender_Age_pridictor:
 
         self.faceProto = "opencv_face_detector.pbtxt"
         self.faceModel = "opencv_face_detector_uint8.pb"
-        self.ageProto = "age_deploy.prototxt"
-        self.ageModel = "age_net.caffemodel"
+        # self.ageProto = "age_deploy.prototxt"
+        # self.ageModel = "age_net.caffemodel"
+        self.ageModel = load_model('before_birth.h5')
         self.genderProto = "gender_deploy.prototxt"
         self.genderModel = "gender_net.caffemodel"
 
         self.MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
         #ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-        self.ageList = ['(0-2)', '(3-6)', '(7-12)', '(13-18)', '(19-29)', '(30-43)', '(44-49)', '(50-100)']
+        self.ageList = ['0-6', '7-18', '19-29', '30-49', '50-']
         #['0-6', '7-18', '19-29', '30-49', '50-']
+        # 0-9, 10-19, 20-29, 
 
         self.genderList = ['M', 'F']
 
@@ -48,31 +52,55 @@ class Gender_Age_pridictor:
                 faceBoxes.append([x1, y1, x2, y2])
                 cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 8)
         return frameOpencvDnn, faceBoxes
-
+    
+    def preprocess_image(self, image, target_size):
+        # 이미지를 Keras 모델 입력 형태로 전처리합니다
+        image = cv2.resize(image, target_size)
+        image = image.astype('float32') / 255.0
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+        return image
+    
     def run(self):
         self.faceNet = cv2.dnn.readNet(self.faceModel, self.faceProto)
-        self.ageNet = cv2.dnn.readNet(self.ageModel, self.ageProto)
+        # self.ageNet = cv2.dnn.readNet(self.ageModel, self.ageProto)
         self.genderNet = cv2.dnn.readNet(self.genderModel, self.genderProto)
 
         video = cv2.VideoCapture(self.args.image if self.args.image else 0)
         
         # # Define dictionaries for gender and age counts
         Most_Gender = {gender: 0 for gender in self.genderList}
-        Most_Age = {age: 0 for age in ['0-6', '7-18', '19-29', '30-49', '50-']}
+        Most_Age = {age: 0 for age in self.ageList}
 
-        Most_Gender, Most_Age = self.loop(video, self.faceNet, self.ageNet, self.genderNet, Most_Gender, Most_Age)
+         # Max retry limit
+        retry_limit = 5
+        retry_count = 0
 
-        most_common_gender = max(Most_Gender, key=Most_Gender.get)
+        while retry_count < retry_limit:
+            Most_Gender, Most_Age = self.loop(video, self.faceNet, self.genderNet, Most_Gender, Most_Age)
+            most_common_gender = max(Most_Gender, key=Most_Gender.get)
+            most_common_age = max(Most_Age, key=Most_Age.get)
 
-        most_common_age = max(Most_Age, key=Most_Age.get)
+            # Check if there are multiple maximum values for gender and age
+            gender_max_count = list(Most_Gender.values()).count(Most_Gender[most_common_gender])
+            age_max_count = list(Most_Age.values()).count(Most_Age[most_common_age])
+
+            if gender_max_count > 1 or age_max_count > 1:
+                print("Warning: Multiple max values found. Retrying...")
+                retry_count += 1
+                Most_Gender = {gender: 0 for gender in self.genderList}
+                Most_Age = {age: 0 for age in self.ageList}
+            else:
+                print("Detection completed")
+                break
 
         print(f'Gender dictionary = {Most_Gender}')
         print(f'Age dictionary = {Most_Age}')
 
-        print(f'-----------------------------------------최종 데이터 값 | Age is {most_common_age} , Gender is {most_common_gender} | -----------------------------------------')
+        print(f'----------------------------------------- 최종 데이터 값 | Age is {most_common_age} , Gender is {most_common_gender} | -----------------------------------------')
 
         if most_common_age and most_common_gender:
-            with open('../latest_age_gender.txt', 'w') as save_custom:
+            with open('latest_age_gender.txt', 'w') as save_custom:
                 save_custom.write(f'{most_common_age}\t')
                 save_custom.write(f'{most_common_gender}')
             print('Success saved\n')
@@ -83,7 +111,7 @@ class Gender_Age_pridictor:
         cv2.destroyAllWindows()
 
 
-    def loop(self, video, faceNet, ageNet, genderNet, Most_Gender, Most_Age):
+    def loop(self, video, faceNet, genderNet, Most_Gender, Most_Age):
         INFO_UPDATE_DURATION = 3  # 업데이트 시간
         face_detected_time = None 
         padding = 20
@@ -101,7 +129,7 @@ class Gender_Age_pridictor:
                 print("No face detected")
                 face_detected_time = None
                 Most_Gender = {gender: 0 for gender in self.genderList}
-                Most_Age = {age: 0 for age in ['0-6', '7-18', '19-29', '30-49', '50-']}
+                Most_Age = {age: 0 for age in self.ageList}
 
             else:
                 current_time = time.time()
@@ -113,40 +141,50 @@ class Gender_Age_pridictor:
                         face = frame[max(0, faceBox[1] - padding):
                                     min(faceBox[3] + padding, frame.shape[0] - 1), max(0, faceBox[0] - padding)
                                     :min(faceBox[2] + padding, frame.shape[1] - 1)]
-
+                        
                         blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), self.MODEL_MEAN_VALUES, swapRB=False)
 
                         genderNet.setInput(blob)
                         genderPreds = genderNet.forward()
                         gender = self.genderList[genderPreds[0].argmax()]
 
+                        print(f"genderPreds ===> {genderPreds} | gender ===> {gender}")
+
                         if gender in Most_Gender:
                             Most_Gender[gender] += 1
 
-                        ageNet.setInput(blob)
-                        agePreds = ageNet.forward()
-                        age = self.ageList[agePreds[0].argmax()]
+                        # ageNet.setInput(blob)
+                        # agePreds = ageNet.forward()
+                        # age = self.ageList[agePreds[0].argmax()]
+                        #ablob = self.preprocess_image(face, (277,277))
+                        ablob = self.preprocess_image(face, (128,128))
+                        agePreds = self.ageModel.predict(ablob)
 
-                        #['(0-2)', '(3-6)', '(7-12)', '(13-18)', '(19-29)', '(30-43)', '(44-49)', '(50-100)']
-                        age_group = ''
-                        if age in ['(0-2)', '(3-6)']:
-                            age_group = '0-6'
-                        elif age in ['(7-12)', '(13-18)']:
-                            age_group = '7-18'
-                        elif age in ['(19-29)']:
-                            age_group = '19-29'
-                        elif age in ['(30-43)', '(44-49)']:
-                            age_group = '30-49'
-                        elif age in ['(50-100)']:
-                            age_group = '50-'
+                        age = self.ageList[np.argmax(agePreds)]
 
-                        if age_group in Most_Age:
-                            Most_Age[age_group] += 1
+                        print(f"agePreds ===> {agePreds} | age ===> {age}")
+                        
+
+                        # #['(0-2)', '(3-6)', '(7-12)', '(13-18)', '(19-29)', '(30-43)', '(44-49)', '(50-100)']
+                        # age_group = ''
+                        # if age in ['(0-6)']:
+                        #     age_group = '0-6'
+                        # elif age in ['(7-18)']:
+                        #     age_group = '7-18'
+                        # elif age in ['(19-29)']:
+                        #     age_group = '19-29'
+                        # elif age in ['(30-49)']:
+                        #     age_group = '30-49'
+                        # elif age in ['(50-']:
+                        #     age_group = '50-'
+
+                        if age in Most_Age:
+                            Most_Age[age] += 1
 
                         print(f'Most Gender --> {Most_Gender} | Most Age --> {Most_Age}')
-                        print(f'Age: {age_group} Gender: {gender}')
+                        print(f'Age: {age} Gender: {gender}')
 
-                        cv2.putText(resultImg, f'{gender}, {age_group}',
+                        cv2.putText(resultImg, f'{gender}, {age}',
                                     (faceBox[0], faceBox[1] - 10),
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
                         
@@ -158,9 +196,6 @@ class Gender_Age_pridictor:
                 break
 
         return Most_Gender, Most_Age
-
-    def custom_age_gender(age, gender):
-        return age, gender
 
 if __name__ == "__main__":
     prediction = Gender_Age_pridictor()
